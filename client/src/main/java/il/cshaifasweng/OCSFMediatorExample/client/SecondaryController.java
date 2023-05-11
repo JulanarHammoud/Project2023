ae0dec9fc4f32a9786933788d91087f842f2b1ca
JulanarHammoud <julanar_h.m@hotmail.com>
2023-05-11 22:49:43 +0300

    1

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SecondaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SecondaryController.java
@@ -4,11 +4,39 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+import static il.cshaifasweng.OCSFMediatorExample.client.App.setRoot;
+
 
 public class SecondaryController {
 
     @FXML
-    private ListView<String> myListView;
+    private ListView<String> myListView= new ListView<>();
+    ObservableList<String> items = FXCollections.observableArrayList();
+   public void initialize(){
+       items.add("Item 1");
+       items.add("Item 2");
+       items.add("Item 3");
 
+       // Set the items in the ListView
+       myListView.setItems(items);
+   }
+    @FXML
+    void clickbutton(ActionEvent event) {
+        if(myListView.getSelectionModel().isEmpty()){
+            System.out.print("You Don't chose Any Clinic\n"+"Please choose one. ");
+        }else {
+            int index=myListView.getSelectionModel().getSelectedIndex();
+            try{
+                setRoot("primary");}
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
