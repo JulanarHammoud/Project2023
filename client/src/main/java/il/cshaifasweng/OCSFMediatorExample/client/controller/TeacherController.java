8cee6dee1fd12d6e6f4b1f433e6fe3011f9fc23c
JulanarHammoud <julanar_h.m@hotmail.com>
2023-06-13 19:20:43 +0300

    Jula

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controller/TeacherController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controller/TeacherController.java
@@ -1,5 +1,5 @@
-package il.cshaifasweng.OCSFMediatorExample.client.controller;
-import il.cshaifasweng.OCSFMediatorExample.client.SimpleClient;
+/*package il.cshaifasweng.OCSFMediatorExample.client.controller;
+import il.cshaifasweng.OCSFMediatorExample.client.EventBus.SimpleClient;
 import il.cshaifasweng.OCSFMediatorExample.entities.*;
 
 import java.util.List;
@@ -16,14 +16,71 @@ public class TeacherController {
 
     int lastIndex= SimpleClient.getParams().size()-1;
     Teacher teacher = (Teacher) SimpleClient.getParams().get(lastIndex);
-    List<CourseTeacher> list =teacher.getCourses();
+    List<SubjectTeacher> list =teacher.getSubjects();
     ObservableList<String> items = FXCollections.observableArrayList();
 
     public void initialize(){
         for(int i=0;i<list.size();i++){
-            items.add(String.valueOf(list.get(i).getName()));}
+            items.add(String.valueOf(list.get(i).getSb_name()));}
         ListViewSubject.setItems(items);
     }
 
+    }*/
+package il.cshaifasweng.OCSFMediatorExample.client.controller;
+
+import il.cshaifasweng.OCSFMediatorExample.client.SimpleClient;
+import il.cshaifasweng.OCSFMediatorExample.entities.Teacher;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import static il.cshaifasweng.OCSFMediatorExample.client.App.setRoot;
+
+public class TeacherController {
+    int lastIndex= SimpleClient.getParams().size()-1;
+    Teacher teacher = (Teacher) SimpleClient.getParams().get(lastIndex);
+    @FXML
+    private Button exams;
+
+    @FXML
+    private Button grades;
+
+    @FXML
+    private Button questions;
+    @FXML
+    private Button subjects;
+
+    @FXML
+    void examsaction(ActionEvent event) {
+        try{
+            SimpleClient.getParams().add(teacher);
+            setRoot("MakeExam");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @FXML
+    void gradesaction(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    void questionaction(ActionEvent event) {
+        try{
+            SimpleClient.getParams().add(teacher);
+            setRoot("ChooseQes");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
 
