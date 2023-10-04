cc43019d6f8153f0cb14ab42e2f42b03ed95529c
lanaloay98 <lanaabbass807@gmail.com>
2023-10-05 00:18:35 +0300

    adding the alerts

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controller/Maneger.java
@@ -0,0 +1,12 @@
+package il.cshaifasweng.OCSFMediatorExample.client.controller;
+
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
+public class Maneger {
+    @FXML
+    public void Question (ActionEvent event){
+
+    }
+}
